632b1a2819051a632e048d47af588749545e8a66
XiaoYunZi1001 <claire_hrz@qq.com>
2018-06-08 23:06:32 +0800

    modify church'ppt question and modify agrithormPOJ3253

--- 算法/第二章/POJ3253_FenceRepair/main.cpp
+++ 算法/第二章/POJ3253_FenceRepair/main.cpp
@@ -8,10 +8,10 @@ int main()
     cin >>N;
     for(int i=0;i<N;i++) cin>>L[i];
     ll ans = 0;
-    //直到计算到木板为1时为止
+    //直到计算到木板为1时为止，木板被切成N快
     while(N > 1)
     {
-        //求出最短板
+        //求出最短板mii1和次短板mii2
         int mii1 = 0, mii2 = 1;
         if(L[mii1] > L[mii2]) swap(mii1,mii2);
         for(int i =2;i<N;i++)
@@ -26,6 +26,7 @@ int main()
                 mii2 = i;
             }
         }
+        //将两块板拼合
         int t = L[mii1] + L[mii2];
         ans += t;
         if(L[mii1] ==N-1) swap(mii1,mii2);
